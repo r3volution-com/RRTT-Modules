154c95d3d989cdaed5ac2778872b2144d155ad14
pablo <pablo@Pablo-Debian.Pablo>
2017-03-02 15:41:16 +0100

    hud

---
+++ HUD/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: pablo
+ *
+ * Created on 2 de marzo de 2017, 15:41
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
