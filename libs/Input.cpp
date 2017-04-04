e54a07815042b955ebc8f626d879c648089031c9
mario <mario@debian>
2017-04-04 20:01:45 +0200

    Input

--- libs/Input.cpp
+++ libs/Input.cpp
@@ -5,3 +5,21 @@ Input::Input() {
 
 Input::~Input() {
 }
+
+void Input::addAction(std::string name, thor::Action act){
+    map[name] = act;
+}
+
+void Input::addActionCallback(std::string name, thor::Action act, std::function< void()> nullaryListener){
+    map[name] = act;
+    system.connect(name, nullaryListener);
+}
+
+void Input::update(sf::Window window){
+    map.update(window);
+    map.invokeCallbacks(system, &window);
+}
+
+bool Input::isActive(std::string name){
+    return map.isActive(name);
+}
--- libs/Input.h
+++ libs/Input.h
@@ -1,12 +1,24 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include <iostream>
+#include <Thor/Input.hpp>
+
 class Input {
     private:
-
+        thor::ActionMap<std::string> map;
+        thor::ActionMap<std::string>::CallbackSystem system;
     public:
         Input();
         virtual ~Input();
+        
+        void addAction(std::string name, thor::Action act);
+        void addActionCallback(std::string name, thor::Action act, std::function< void()> nullaryListener);
+        
+        void update(sf::Window window);
+        void callbacks(sf::Window window);
+        
+        bool isActive(std::string name);
 };
 
 #endif /* INPUT_H */
