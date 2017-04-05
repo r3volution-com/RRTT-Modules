d2fa62bf3e915543649d6ebc58d6063f9f05d114
ismael <ismael@debian.ismael>
2017-04-05 10:19:58 +0200

    comentario input

--- libs/Input.h
+++ libs/Input.h
@@ -12,12 +12,38 @@ class Input {
         Input();
         virtual ~Input();
         
+        /**
+         * Anade una accion
+         * @param name: Nombre de la accion
+         * @param act: Tecla a pulsar
+         */
         void addAction(std::string name, thor::Action act);
+        
+        /**
+         * Anadir una accion que ejecuta una funcion al cumplirse
+         * @param name: Nombre de la accion
+         * @param act: Tecla a pulsar
+         * @param nullaryListener: Accion a realizar
+         */
         void addActionCallback(std::string name, thor::Action act, std::function< void()> nullaryListener);
         
+        /**
+         * Comprueba las acciones registradas
+         * @param window: Ventana
+         */
         void update(sf::Window window);
+        
+        /**
+         * Invoca a todos los callbacks registrados
+         * @param window: Ventana
+         */
         void callbacks(sf::Window window);
         
+        /**
+         * Comprueba si se esta pulsando esa combinacion de teclas
+         * @param name: Teclas
+         * @return 
+         */
         bool isActive(std::string name);
 };
 
