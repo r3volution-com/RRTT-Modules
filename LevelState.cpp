45eae556ca42149f5bb6f70bdbf3968a3753d142
pablo <pablo@fv.fv>
2017-05-11 17:38:29 +0200

    Merge origin/master
    Conflicts:
    	LevelState.cpp

--- LevelState.cpp
+++ LevelState.cpp
@@ -95,6 +95,7 @@ void LevelState::Init(){
     
     rath->addGun(gunArm);
     rath->changeGun(0);
+        rath->setPosition(Coordinate(4500, 10300));
     
     /*****LEVEL*****/
     level = new Level(1);
