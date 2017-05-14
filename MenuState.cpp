b193e7a1f9d7010fb8174ab83613b9d2fc15ad26
sergio <sergio@sergio-EasyNote-LJ75>
2017-05-14 20:11:09 +0200

    Menu principal

--- MenuState.cpp
+++ MenuState.cpp
@@ -17,13 +17,13 @@ void MenuState::Init(){
     game->rM->loadTexture("button-layout", "resources/button-layout.png");
     game->rM->loadFont("menu", "resources/font.ttf");
     principal = new Menu(game->rM->getTexture("menu-background"), game->rM->getTexture("button-layout"), new Rect<float>(0,0,200,50), game->rM->getFont("menu"));
-    principal->addButton(Coordinate (540,200), "Jugar", sf::Color::Black, sf::Color::Transparent, 20);
-    principal->addButton(Coordinate (540,270), "Opciones", sf::Color::Black, sf::Color::Transparent, 20);
-    principal->addButton(Coordinate (540,340), "Salir", sf::Color::Black, sf::Color::Transparent, 20);
+    principal->addButton(Coordinate (770,270), "Jugar", sf::Color::Black, sf::Color::Transparent, 20);
+    principal->addButton(Coordinate (770,340), "Opciones", sf::Color::Black, sf::Color::Transparent, 20);
+    principal->addButton(Coordinate (770,410), "Salir", sf::Color::Black, sf::Color::Transparent, 20);
     opciones = new Menu(game->rM->getTexture("menu-background"), game->rM->getTexture("button-layout"), new Rect<float>(0,0,200,50), game->rM->getFont("menu"));
-    opciones->addButton(Coordinate (540,200), "Musica On/Off", sf::Color::Black, sf::Color::Transparent, 20);
-    opciones->addButton(Coordinate (540,270), "FX On/Off", sf::Color::Black, sf::Color::Transparent, 20);
-    opciones->addButton(Coordinate (540,340), "Volver", sf::Color::Black, sf::Color::Transparent, 20);
+    opciones->addButton(Coordinate (770,270), "Musica On/Off", sf::Color::Black, sf::Color::Transparent, 20);
+    opciones->addButton(Coordinate (770,340), "FX On/Off", sf::Color::Black, sf::Color::Transparent, 20);
+    opciones->addButton(Coordinate (770,410), "Volver", sf::Color::Black, sf::Color::Transparent, 20);
     game->iM->addAction("click", thor::Action(sf::Mouse::Left));
     
     menuactual = 0;
